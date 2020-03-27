a72146ca1981feec47625f711cfdc0abb3e2ddbb
Nail <naillutfullin777@gmail.com>
2020-03-27 08:00:41 +0600

    showUser add

---
+++ showUser/showUser.js
@@ -0,0 +1,5 @@
+let age = document.getElementById('age');
+function showUser(surname, name) {
+	alert("Пользователь " + surname + " " + name + ", его возраст " + age.value);
+}
+showUser('Наиль', 'Лутфуллин');
