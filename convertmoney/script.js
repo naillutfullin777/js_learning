5f76d604553a1ea04992d934f47b7138bcca39a9
Nail <naillutfullin777@gmail.com>
2020-04-01 17:34:22 +0600

    convertmoney: add promise

--- convertmoney/script.js
+++ convertmoney/script.js
@@ -1,21 +1,29 @@
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
-inputRub.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
+inputRub.addEventListener('input', function() {
+     let promise = new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
 
-    request.open('GET', 'current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
+        request.open('GET', 'current.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
-
-            inputUsd.value = inputRub.value / data.usd;
-        } else {
-            inputUsd.value = "Что-то пошло не так!";
-        }
+        request.onload = function() {
+            if (request.readyState === 4 && request.status == 200) {
+                let data = JSON.parse(request.response);
+                resolve(inputRub.value / data.usd);
+            } else {
+                reject(new Error(request.statusText));
+            }
+        };
+        request.onerror = function() {
+            reject(Error("Сетевая ошибка"));
+        };
+        request.send();
     });
-
+    promise
+        .then(
+            result => inputUsd.value = result, 
+            error => inputUsd.value =  error
+        )
 });
